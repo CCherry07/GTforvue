5c3d61be55d762b03519a2f75c9cbcd075713c8a
CJG-hub <2405693142@qq.com>
2021-05-13 14:23:07 +0800

    更新

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+
 const program = require('commander');
 const helpOptions =  require('./lib/core/help')
 const createCommands = require('./lib/core/create')
-program.version(require('./package-lock.json').version);
-program.version(require('./package-lock.json').version ,'-v -version');
+program.version(require('./package.json').version);
+program.version(require('./package.json').version ,'-v -version');
 helpOptions()
 // 创建命令行
 createCommands()
